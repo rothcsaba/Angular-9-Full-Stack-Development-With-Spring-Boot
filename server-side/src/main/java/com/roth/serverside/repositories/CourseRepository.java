4895476ce2e869819341ff7537873f70cb004ee8
roth_csaba <csaba231093@gmail.com>
2020-03-31 18:40:30 +0200

    001: CourseRepository

--- server-side/src/main/java/com/roth/serverside/repositories/CourseRepository.java
+++ server-side/src/main/java/com/roth/serverside/repositories/CourseRepository.java
@@ -1,12 +1,7 @@
 package com.roth.serverside.repositories;
 
 import com.roth.serverside.models.Course;
-import com.roth.serverside.models.CourseStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CourseRepository extends JpaRepository<Course, Long> {
-
-    Optional<CourseStudent> findByCourseInstructorId(Long instructorId);
 }
