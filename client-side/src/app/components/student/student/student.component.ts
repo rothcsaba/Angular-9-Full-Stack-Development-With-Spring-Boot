ffcff7a159eda64042203ee1624d0e4163a0126b
roth_csaba <csaba231093@gmail.com>
2020-04-01 18:48:45 +0200

    002: Student Page implementation

--- client-side/src/app/components/student/student/student.component.ts
+++ client-side/src/app/components/student/student/student.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../../models/user';
+import {Course} from '../../../models/course';
+import {ActivatedRoute} from '@angular/router';
+import {UserService} from '../../../services/user.service';
 
 @Component({
   selector: 'app-student',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  studentId: string;
+  currentStudent: User;
+  courseList: Array<Course>;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private userService: UserService) {
+    this.currentStudent = JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      if (params.has('id')) {
+        this.studentId = params.get('id');
+      }
+
+      if (this.studentId || this.currentStudent) {
+        this.findAllCoursesOfStudent();
+      }
+    });
+  }
+
+  findAllCoursesOfStudent() {
+    if (!this.studentId) {
+      this.studentId = this.currentStudent.id.toString();
+    }
+    this.userService.findAllCoursesOfStudent(this.studentId).subscribe(data => {
+      this.courseList = data;
+    });
   }
 
 }
