b1401d218299a3b5fcb3bfb1acea2d5b0716ad7b
roth_csaba <csaba231093@gmail.com>
2020-04-01 19:17:30 +0200

    002: Navigation bar

--- client-side/src/app/app.component.ts
+++ client-side/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {UserService} from './services/user.service';
+import {User} from './models/user';
+import {Role} from './models/role';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client-side';
+  currentUser: User;
+
+  constructor(private userService: UserService, private router: Router) {
+    this.userService.currentUser.subscribe(data => {
+      this.currentUser = data;
+    });
+  }
+
+  logOut() {
+    this.userService.logOut().subscribe(data => {
+      this.router.navigate(['/login']);
+    });
+  }
+
+  get isStudent() {
+    return this.currentUser && this.currentUser.role === Role.STUDENT;
+  }
+
+  get isTeacher() {
+    return this.currentUser && this.currentUser.role === Role.TEACHER;
+  }
+
+  get isManager() {
+    return this.currentUser && this.currentUser.role === Role.MANAGER;
+  }
 }
