e1cc4f6f4bc4f37d54ba16f0838cb33f78c91221
roth_csaba <csaba231093@gmail.com>
2020-04-01 18:15:09 +0200

    002: Register page implementation

--- client-side/src/app/components/user/register/register.component.ts
+++ client-side/src/app/components/user/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../../services/user.service';
+import {User} from '../../../models/user';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+  errorMessage: string;
 
-  ngOnInit(): void {
+  constructor(private userService: UserService, private router: Router) {
+  }
+
+  ngOnInit() {
+    if (this.userService.currentUserValue) {
+      this.router.navigate(['/home']);
+      return;
+    }
+  }
+
+  register() {
+    this.userService.register(this.user).subscribe(data => {
+      this.router.navigate(['/login']);
+    }, err => {
+      this.errorMessage = 'Username is already exist';
+    });
   }
 
 }
