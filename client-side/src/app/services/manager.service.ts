c7ed7603902174eaf453ad8de14f82cfb32faa23
roth_csaba <csaba231093@gmail.com>
2020-04-01 19:08:03 +0200

    002: Manager Service implementation

--- client-side/src/app/services/manager.service.ts
+++ client-side/src/app/services/manager.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../models/user';
+
+const API_URL = 'http://localhost:8080/api/manager/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManagerService {
 
-  constructor() { }
+  currentUser: User;
+  headers: HttpHeaders;
+
+  constructor(private http: HttpClient) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.headers = new HttpHeaders({
+      authorization: 'Bearer ' + this.currentUser.token,
+      'Content-Type': 'application/json; charset=UTF-8'
+    });
+  }
+
+  // REST API settings: @GetMapping("/api/manager/enrollments")
+  findAllEnrollments(): Observable<any> {
+    return this.http.get(API_URL + 'enrollments', {headers: this.headers});
+  }
 }
